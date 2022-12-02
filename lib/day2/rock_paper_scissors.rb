aa19336d64498074d0d01daa8fb538febb92bf96
Lee Ourand <lee.ourand@vts.com>
2022-12-02 15:11:18 -0500

    Day2 Challenge solution 2

--- lib/day2/rock_paper_scissors.rb
+++ lib/day2/rock_paper_scissors.rb
@@ -5,10 +5,23 @@ class StrategyGuide
     @guide = guide
   end
 
-  def total_score
-    parse_guide.reduce(0) do |a, round|
-      a + THROW_SCORES[round.my_throw] + score_for_result_of_round(round)
+  def assumed_score
+    assumed_translation = Proc.new do |oppo_throw, my_throw|
+      map = { "A" => :rock, "B" => :paper, "C" => :scissors, "X" => :rock, "Y" => :paper, "Z" => :scissors }
+      Round.new(map[my_throw], map[oppo_throw])
     end
+    total_score(assumed_translation)
+  end
+
+  def informed_score
+    informed_translation = Proc.new do |oppo_throw, condition|
+      throw_map = { "A" => :rock, "B" => :paper, "C" => :scissors }
+      condition_map = { "X" => :lose, "Y" => :draw, "Z" => :win }
+      translated_oppo_throw = throw_map[oppo_throw]
+      translated_condition = condition_map[condition]
+      Round.new(throw_needed_for_condition(translated_oppo_throw, translated_condition), translated_oppo_throw)
+    end
+    total_score(informed_translation)
   end
 
   private
@@ -27,6 +40,23 @@ class StrategyGuide
     scissors: :paper
   }
 
+  def throw_needed_for_condition(opponent_throw, condition)
+    case condition
+    when :lose
+      WINNER_LOSER_RELATIONSHIPS[opponent_throw]
+    when :draw
+      opponent_throw
+    else
+      (WINNER_LOSER_RELATIONSHIPS.keys - [opponent_throw] - [WINNER_LOSER_RELATIONSHIPS[opponent_throw]]).first
+    end
+  end
+
+  def total_score(throw_translation)
+    parse_guide(throw_translation).reduce(0) do |a, round|
+      a + THROW_SCORES[round.my_throw] + score_for_result_of_round(round)
+    end
+  end
+
   def score_for_result_of_round(round)
     { lose: 0, draw: 3, win: 6 }[result_of_round(round)]
   end
@@ -37,15 +67,13 @@ class StrategyGuide
     return :lose
   end
 
-  def parse_guide
-    translation = { "A" => :rock, "B" => :paper, "C" => :scissors, "X" => :rock, "Y" => :paper, "Z" => :scissors }
-    guide.map do |round|
-      Round.new(translation[round[1]], translation[round[0]])
-    end
+  def parse_guide(translation)
+    guide.map(&translation)
   end
 end
 
 if __FILE__ == $0
   guide = File.read(File.join(__dir__, "inputs/input.txt")).split("\n").map {|line| line.chomp.split }
-  puts StrategyGuide.new(guide).total_score
+  puts "Assumed Score: #{StrategyGuide.new(guide).assumed_score}"
+  puts "Informed Score: #{StrategyGuide.new(guide).informed_score}"
 end
--- test/day2_test.rb
+++ test/day2_test.rb
@@ -2,15 +2,15 @@ require 'minitest/autorun'
 require_relative '../lib/day2/rock_paper_scissors'
 
 class Day2Test < MiniTest::Test
-  def test_building_elves_from_string
-    guide = [["A", "Y"], ["B", "X"], ["C", "Z"]]
-    total_score = StrategyGuide.new(guide).total_score
-    assert_equal 15, total_score
+  def test_building_elves_from_assumed_string
+    guide = [["B", "Y"], ["C", "X"], ["A", "Z"], ["B", "X"]]
+    total_score = StrategyGuide.new(guide).assumed_score
+    assert_equal (5 + 7 + 3 + 1), total_score
   end
 
-  def test_building_elves_from_string
-    guide = [["B", "Y"], ["C", "X"], ["A", "Z"], ["B", "X"]]
-    total_score = StrategyGuide.new(guide).total_score
-    assert_equal (5 + 7 + 3 + 1), total_score
+  def test_building_elves_from_informed_string
+    guide = [["A", "Y"], ["B", "X"], ["C", "Z"]]
+    total_score = StrategyGuide.new(guide).informed_score
+    assert_equal 12, total_score
   end
 end
